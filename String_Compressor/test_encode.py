65b214d9f5b2fc8f53f41ccd6fbfabb3f245010f
Multezem Kedir <kedirmultezem@gmail.com>
2019-10-01 23:22:08 -0700

    added test case for after compression

--- String_Compressor/test_encode.py
+++ String_Compressor/test_encode.py
@@ -9,3 +9,12 @@ def test_empty_string():
 
 def test_normal_string():
     assert encode("ad") == "ad"
+
+
+def test_normal_string():
+    assert encode("ad") == "ad"
+
+
+def test_compressed_string():
+    assert encode("Aabb") == "Aab2"
+    assert encode("AAb") == "A2b"
