ecb0c0efcb8509b7ee78c4f8d1ed48e53e4f8b21
Multezem Kedir <kedirmultezem@gmail.com>
2019-10-01 23:41:24 -0700

    not tested encode function

--- String_Compressor/encode.py
+++ String_Compressor/encode.py
@@ -6,4 +6,17 @@ def encode(input_string):
     :param input_string:
     :return:
     """
+    # create new string
+    result = ""
+
+    # get each letter
+    for i in input_string:
+        # count occurrences
+        count = input_string.count(i)
+        # add letter to the new string
+        result += i
+        # add occurrences if more than one
+        if count != 1:
+            result += str(count)
+
     return input_string
